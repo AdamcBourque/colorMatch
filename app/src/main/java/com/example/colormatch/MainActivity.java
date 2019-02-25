9366b49e5a33bdc1b6b3781517c309300986ad59
AdamcBourque <adamobourque@gmail.com>
2019-02-25 15:44:03 -0500

    autohide keyboard and add listener

--- app/src/main/java/com/example/colormatch/MainActivity.java
+++ app/src/main/java/com/example/colormatch/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.colormatch;
 
+import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,8 +20,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListener();
     }
-
+    private void addListener(){
+        EditText.OnEditorActionListener listener = new EditText.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction(TextView v, int actionID, KeyEvent event){
+                if (actionID == EditorInfo.IME_ACTION_DONE){
+                    buttonClick(null);
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        };
+        EditText input = findViewById(R.id.input);
+        input.setOnEditorActionListener(listener);
+    }
     /**
      * Get text form input and set background to that color
      * @param view
@@ -25,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         EditText input = findViewById(R.id.input);
         String name = input.getText().toString().toUpperCase();
         setColorByName(name);
+        hideKeyboardFrom(getApplicationContext(), getCurrentFocus());
     }
     private void setColorByName(String c){
         TextView display = findViewById(R.id.output);
@@ -35,5 +56,9 @@ public class MainActivity extends AppCompatActivity {
         }else{
             Toast.makeText(this,"Your color is wrong", Toast.LENGTH_SHORT).show();
         }
+    }
+    public void hideKeyboardFrom(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
